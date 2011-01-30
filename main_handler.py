15d98434181435939343abfa03ee43aa50440738
diptanu <admin.nitjece@gmail.com>
2011-01-30 21:22:36 +0530

    Making the bookmarks show

--- main_handler.py
+++ main_handler.py
@@ -21,29 +21,5 @@ class MainHandler(webapp.RequestHandler):
         template_values = {'user_name': username, 'logout_url': logout_url, 'urls': urls}
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
         self.response.out.write(template.render(path, template_values))
-        
-        
-    def post(self):
-        user = users.get_current_user()
-        if user is None:
-            self.redirect('/')
-        url = self.request.get('url_text')
-        tags = self.request.get('url_tag').split(",")
 
-        usable_tags = []
-        for tag_name in tags:
-            if len(tag_name) is 0:
-                continue
-            tag = Tag(name = tag_name)
-            tags_query = Tag.all()
-            tags_query.filter('name =', tag_name)
-            
-            if len(tags_query.fetch(1)) is not 0:
-                continue
-            tag.put()
-            usable_tags.append(tag.key())
-        
-        book_mark = Bookmark(user = user, url = url, tags = usable_tags)
-        book_mark.put()
-        self.redirect('/')
         
